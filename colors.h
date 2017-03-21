e4b5c196524c92bebd2b5d0584552494b2cdcd61
Keegan Donley <keegan.donley@gmail.com>
2017-03-20 20:20:56 -0700

    add more styling

--- colors.h
+++ colors.h
@@ -2,5 +2,11 @@
 #define GREEN "\033[1;32m"
 #define RED "\033[1;31m"
 #define PINK "\033[1;35m"
+#define BLACK "\033[1;30m"
+#define YELLOW "\033[1;33m"
+#define CYAN "\033[1;36m"
+#define WHITE "\033[1;37m"
+#define UNDERLINE "\033[4m"
+#define BLINK "\033[5m"
 
 #define ENDCOLORS "\033[0m"
